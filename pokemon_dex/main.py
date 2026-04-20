cecb13d7929d47e20e47dee29977845492fb48fc
SlowerTrick <arturvitorcintra@gmail.com>
2026-04-20 19:12:16 -0300

    better output visualization

--- pokemon_dex/main.py
+++ pokemon_dex/main.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 
 TOTAL = 30
 types_list = []
+pokemon_list = []
 
 for _ in range(TOTAL):
     pokemon = random_pokemon()
     if pokemon:
+        pokemon_list.append(pokemon)
         types_list.extend(pokemon["types"])
 
 text = " ".join(types_list)
 print(text)
+
+for pokemon in pokemon_list:
+    print(f"{pokemon['name'].title()}: {', '.join(t.title() for t in pokemon['types'])}")
 
 wc = WordCloud(width=800, height=400, background_color="white").generate(text)
 
